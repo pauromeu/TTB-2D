355bc1662800a5267b0851261fb4d5c75fe2ef33
Mengjie Zhao <mengjiezhao@outlook.de>
2024-04-19 13:58:44 +0200

    add notes

--- generate_healthy_data.m
+++ generate_healthy_data.m
@@ -1,4 +1,6 @@
 % Script to generate 200 healthy samples for a train infrastructure simulation
+% Note that we generate continous damage factor values between 0.7 and 1.0, whereas in the original PTAE paper, the damage factor is discretized into 3 levels: 0.9, 0.8 and 0.7.
+
 
 % Constants for the simulation
 damage_factor = 1.0;  % Assuming damage factor of 1 implies a healthy condition
