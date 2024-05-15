bf5240f30c162492634c5551da5459cbca0153d8
Pau Romeu <romeu.pau@gmail.com>
2024-05-15 19:19:22 +0200

    add different params

--- generate_comparative_data.m
+++ generate_comparative_data.m
@@ -18,11 +18,11 @@ VehicleArgs.load_std = 0.0;
 
 BridgeArgs = struct();
 BridgeArgs.damage_factor = 0.7;
-BridgeArgs.damage_center_location = 0.2;
+BridgeArgs.damage_center_location = 0.75;
 BridgeArgs.damage_num_elements = 2;
 BridgeArgs.damage_local = true;
 BridgeArgs.num_sensors = 10;
-BridgeArgs.elastic_module_randomness = false;
+BridgeArgs.elastic_module_randomness = true;
 
 EnvironmentArgs = struct();
 EnvironmentArgs.temperature_C = 20.0;
@@ -31,13 +31,28 @@ SimulationArgs = struct();
 SimulationArgs.freq = 1000;
 
 FileArgs = struct();
-FileArgs.file_name = 'comparative.csv';
+FileArgs.file_name = 'comparative_temps.csv';
 FileArgs.date = datetime('now');
 FileArgs.sample_number = -1; % -1 for automatic numbering
 
-damage_factors = 0.6:0.1:1.0;
+damage_factors = 0.7:0.1:1.0;
+temperatures = 5:5:30;
+velocities_m_s = 40:20:100;
+repeat = 3;
+
+disp('Starting simulation ...');
+disp('Computing' + string(length(damage_factors) * length(temperatures) * length(velocities_m_s) * repeat) + ' samples.');
 
 for i = 1:length(damage_factors)
     BridgeArgs.damage_factor = damage_factors(i);
-    simulate_single_pass(VehicleArgs, BridgeArgs, EnvironmentArgs, SimulationArgs, FileArgs);
+    for v = 1:length(velocities_m_s)
+        VehicleArgs.velocity_m_s = velocities_m_s(v);
+        for j = 1:length(temperatures)
+            EnvironmentArgs.temperature_C = temperatures(j);
+            for r = 1:repeat
+                disp(['Damage factor: ', num2str(damage_factors(i)), ' - Temperature: ', num2str(temperatures(j)), ' - Repeat: ', num2str(r)]);
+                simulate_single_pass(VehicleArgs, BridgeArgs, EnvironmentArgs, SimulationArgs, FileArgs);
+            end
+        end
+    end
 end
