e0567aebf597b630e8ced35a3d2af269f1abbf22
Pau Romeu <romeu.pau@gmail.com>
2024-05-15 11:26:30 +0200

    set solver time step as freq param

--- functions/simulate_single_pass.m
+++ functions/simulate_single_pass.m
@@ -8,6 +8,11 @@ function simulate_single_pass(VehicleArgs, BridgeArgs, EnvironmentArgs, Simulati
 
     % Set options with a predefined function
     A04_Options;
+    
+    if Calc.Solver.max_accurate_frq ~= SimulationArgs.freq
+        Calc.Solver.max_accurate_frq = SimulationArgs.freq
+        disp("Using solver freq of " + SimulationArgs.freq + "Hz.")
+    end
 
     % -- Model geometry --
     [Calc,Train,Beam] = B43_ModelGeometry(Calc, Train, Track, Beam);
@@ -21,6 +26,17 @@ function simulate_single_pass(VehicleArgs, BridgeArgs, EnvironmentArgs, Simulati
     % Add effect of damage
     [Beam] = set_bridge_localized_damage(Beam, BridgeArgs);
 
+    % Run simulation
+    [Sol, Model, Calc, Train, Track, Beam] = run_simulation(Train, Track, Beam, Calc);
 
+     % Write out solution to a CSV file
+     file_name = 'healthy.csv';
+     if damage_factor ~= 1
+         file_name = 'damaged.csv';
+     end
+ 
+     write_result(Sol, N, file_name, ...
+         damage_factor, T, train_init_velocity, ET,...
+         sample_number);
 end
 
